6897ad4acc2f01f444466ccef8255f36de23b46b
lilei6809 <lilei6809@gmail.com>
2024-10-24 16:17:50 +0100

    LanlanMQ first commit

---
+++ LanlanMQ/src/main/java/com/leland/lanlanmq/LanlanMqApplication.java
@@ -0,0 +1,13 @@
+package com.leland.lanlanmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanlanMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanlanMqApplication.class, args);
+    }
+
+}
---
+++ LanlanMQ/src/test/java/com/leland/lanlanmq/LanlanMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leland.lanlanmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanlanMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
